5756ef9ff9b3133dabf1a5a6d9fbe238361a7e92
JinsuBae2 <bjs7898@naver.com>
2026-01-29 13:16:20 +0900

    Fix: Restore missing index.css import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import ReactGA from 'react-ga4';
 
 ReactGA.initialize('G-FNMV246B74');
