a17b3672fd3087193a5248bca94fc30a01b1d41b
JinsuBae2 <bjs7898@naver.com>
2026-01-29 13:08:46 +0900

    Feat: Add Google Analytics 4 (GA4) integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
 import ResultPage from './pages/ResultPage';
 import Layout from './components/Layout';
 
+// 페이지 이동 추적을 위한 컴포넌트
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <PageTracker />
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-FNMV246B74');
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
